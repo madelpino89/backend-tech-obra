b5c07879172cdada6a0a69863baf5c6cc3137b21
madelpino89 <miguel-angel.pino@ext.leroymerlin.es>
2024-03-14 14:58:49 +0100

    [BACKEND][Added Swagger]

--- src/main/java/com/obramat/app/AppApplication.java
+++ src/main/java/com/obramat/app/AppApplication.java
@@ -1,7 +1,11 @@
 package com.obramat.app;
 
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppApplication {
@@ -10,4 +14,8 @@ public class AppApplication {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return  new OpenAPI().info(new Info().title("OBRAMAT TECHNICAL TEST API").version("1.0"));
+	}
 }
